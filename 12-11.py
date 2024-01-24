2c5f9e19075b8268412b9f3f880d59385ab187b2
Artur <fhnehbcvfubkjd@yandex.ru>
2024-01-25 03:08:32 +0500

    12-11

---
+++ 12-11.py
@@ -0,0 +1,11 @@
+town1 = input()
+town2 = input()
+
+tula = 'Тула'
+penz = 'Пенза'
+
+if town1 != town2 and (town1 == tula and town2 != penz or
+                       town2 == penz and town1 != tula):
+    print('ДА')
+else:
+    print('НЕТ')
